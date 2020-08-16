4abe9f702efb8e929abc3df7cdc938728195bcbd
Luiz Lima <umluizlima@gmail.com>
2020-08-16 15:50:49 -0300

    Rename posts to artigos (#1)

--- pages/index.js
+++ pages/index.js
@@ -26,11 +26,11 @@ export default function Home({ allPostsData }) {
       </section>
       {allPostsData.length > 0 &&
         <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-          <h2 className={utilStyles.headingLg}>Blog</h2>
+          <h2 className={utilStyles.headingLg}>Artigos</h2>
           <ul className={utilStyles.list}>
             {allPostsData.map(({ id, date, title }) => (
               <li className={utilStyles.listItem} key={id}>
-                <Link href="/posts/[id]" as={`/posts/${id}`}>
+                <Link href="/artigos/[id]" as={`/artigos/${id}`}>
                   <a>{title}</a>
                 </Link>
                 <br />
