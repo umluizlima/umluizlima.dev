a884413254a15c49f114eb3f42926ba7fa2bdf01
umluizlima <umluizlima@gmail.com>
2020-08-27 23:39:32 -0300

    Add disclaimer component

---
+++ src/components/disclaimer/index.js
@@ -0,0 +1,8 @@
+const Disclaimer = () => (
+  <p>
+    <strong>Observações:</strong> este conteúdo pode incluir links para programas de
+    afiliados a partir dos quais posso receber uma pequena comissão caso você realize uma compra.
+  </p>
+)
+
+export default Disclaimer
