6ac2d04c5405f0410ae1d1602f8687c1c6248a8e
umluizlima <umluizlima@gmail.com>
2020-08-29 20:59:14 -0300

    Move home content to markdown

---
+++ content/home.md
@@ -0,0 +1,9 @@
+Olá! Me chamo **Luiz** e sou um Engenheiro de Software brasileiro.
+
+Neste site você pode acompanhar minhas **publicações**.
+
+Para conhecer mais detalhes de minha **carreira**, recomendo que nos conectemos através do <a target="_blank" rel="noopener" href="https://www.linkedin.com/in/umluizlima">LinkedIn</a>.
+
+Se quiser explorar meus <strong>projetos</strong>, me siga no <a target="_blank" rel="noopener" href="https://github.com/umluizlima">GitHub</a>.
+
+Também estou disponível para contato em outras redes sociais, como <a target="_blank" rel="noopener" href="https://facebook.com/umluizlima">Facebook</a> ou <a target="_blank" rel="noopener" href="https://twitter.com/umluizlima">Twitter</a>.
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,17 @@
 import Articles from '../components/articles'
 import Layout from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
-import { getSortedContentData, getBlogDirectory } from '../utils/content'
+import {
+  getSortedContentData,
+  getBlogDirectory,
+  getContentData,
+  getContentDirectory,
+} from '../utils/content'
 
-const Home = ({ articles }) => (
+const Home = ({ articles, home }) => (
   <Layout home>
     <section className={utilStyles.headingMd}>
-      <p>
-        Olá! Me chamo <strong>Luiz</strong> e sou um Engenheiro de Software brasileiro.
-      </p>
-      <p>
-        Neste site você pode acompanhar minhas <strong>publicações</strong>.
-      </p>
-      <p>
-        Para conhecer mais detalhes de minha <strong>carreira</strong>, recomendo que nos conectemos através do{' '}
-        <a target="_blank" rel="noopener" href="https://www.linkedin.com/in/umluizlima">LinkedIn</a>.
-      </p>
-      <p>
-        Se quiser explorar meus <strong>projetos</strong>, me siga no{' '}
-        <a target="_blank" rel="noopener" href="https://github.com/umluizlima">GitHub</a>.
-      </p>
-      <p>
-        Também estou disponível para contato em outras redes sociais, como{' '}
-        <a target="_blank" rel="noopener" href="https://facebook.com/umluizlima">Facebook</a>{' '}
-        ou <a target="_blank" rel="noopener" href="https://twitter.com/umluizlima">Twitter</a>.
-      </p>
+      <div dangerouslySetInnerHTML={{ __html: home.content }} />
     </section>
     <Articles articles={articles} styles={utilStyles} />
   </Layout>
@@ -32,6 +19,7 @@ const Home = ({ articles }) => (
 
 export const getStaticProps = async () => ({
   props: {
+    home: getContentData(getContentDirectory(), 'home'),
     articles: getSortedContentData(getBlogDirectory())
   },
 })
