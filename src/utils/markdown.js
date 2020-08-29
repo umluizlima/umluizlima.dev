9c387993697ff1aacc470eb32b2bbefa5a227e04
umluizlima <umluizlima@gmail.com>
2020-08-29 20:15:41 -0300

    Refactor content utils

---
+++ src/utils/markdown.js
@@ -0,0 +1,24 @@
+const fs = require('fs')
+const matter = require('gray-matter')
+const { highlight, getLanguage } = require('highlight.js')
+const marked = require('marked')
+
+const readMarkdownFile = (fullpath) => {
+  const fileContents = fs.readFileSync(fullpath, 'utf8')
+  return matter(fileContents)
+}
+
+const parseMarkdownContent = (content) => {
+  marked.setOptions({
+    highlight: (code, language) => {
+      const validLanguage = getLanguage(language) ? language : 'plaintext'
+      return highlight(validLanguage, code).value
+    }
+  })
+  return marked(content)
+}
+
+module.exports = {
+  readMarkdownFile,
+  parseMarkdownContent,
+}
