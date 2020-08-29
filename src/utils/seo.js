1e1a84d047f03b0f030053785fd854f637121611
umluizlima <umluizlima@gmail.com>
2020-08-29 17:45:45 -0300

    Add base SEO definitions

---
+++ src/utils/seo.js
@@ -0,0 +1,25 @@
+export default {
+  title: 'Início',
+  titleTemplate: '%s | umluizlima',
+  description: 'Site pessoal de umluizlima',
+  canonical: 'https://umluizlima.dev',
+  openGraph: {
+    type: 'website',
+    locale: 'pt_BR',
+    url: 'https://umluizlima.dev',
+    site_name: 'umluizlima',
+    images: [
+      {
+        url: 'https://og-image.now.sh/umluizlima.png?theme=light&md=0&fontSize=75px',
+        width: 800,
+        height: 600,
+        alt: 'umluizlima website cover',
+      },
+    ],
+  },
+  twitter: {
+    handle: '@umluizlima',
+    site: '@umluizlima',
+    cardType: 'summary_large_image',
+  },
+};
