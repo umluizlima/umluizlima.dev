b524beddc6c429fecbc8ae5ffd4277eedf944906
umluizlima <umluizlima@gmail.com>
2021-03-30 16:49:51 -0300

    Add GoatCounter script

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import { Fragment } from 'react'
 import { DefaultSeo } from 'next-seo'
+import Head from 'next/head'
 
 import '../styles/code.css'
 import '../styles/globals.css'
@@ -8,6 +9,9 @@ import SEO from '../utils/seo'
 const MyApp = ({ Component, pageProps }) => (
   <Fragment>
     <DefaultSeo {...SEO} />
+    <Head>
+      <script data-goatcounter="https://umluizlima.goatcounter.com/count" async src="//gc.zgo.at/count.js"></script>
+    </Head>
     <Component {...pageProps} />
   </Fragment>
 )
