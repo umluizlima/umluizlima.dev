75a6b920c875feb9b695b163bc98ed708aee3377
Luiz Lima <umluizlima@gmail.com>
2022-01-03 21:53:22 -0300

    Update goatcounter integration (#20)

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,19 +1,40 @@
-import { Fragment } from 'react'
+import { Fragment, useEffect } from 'react'
 import { DefaultSeo } from 'next-seo'
 import Head from 'next/head'
+import { useRouter } from 'next/router'
 
 import '../styles/code.css'
 import '../styles/globals.css'
 import SEO from '../utils/seo'
 
-const MyApp = ({ Component, pageProps }) => (
-  <Fragment>
-    <DefaultSeo {...SEO} />
-    <Head>
-      <script data-goatcounter="https://umluizlima.goatcounter.com/count" async src="//gc.zgo.at/count.js"></script>
-    </Head>
-    <Component {...pageProps} />
-  </Fragment>
-)
+const MyApp = ({ Component, pageProps }) => {
+  const router = useRouter()
+
+  useEffect(() => {
+    window.goatcounter = { no_onload: true }
+  }, [])
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+        window.goatcounter.count({
+          path: url,
+      })
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
+  return (
+    <Fragment>
+      <DefaultSeo {...SEO} />
+      <Head>
+        <script data-goatcounter="https://umluizlima.goatcounter.com/count" async src="//gc.zgo.at/count.js"></script>
+      </Head>
+      <Component {...pageProps} />
+    </Fragment>
+  )
+}
 
 export default MyApp
